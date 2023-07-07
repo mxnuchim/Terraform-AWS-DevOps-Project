8db3f28731d11bc6fa8e2214ea21e886dfe4b38f
mxnuchim <manuchimoliver779@gmail.com>
2023-07-07 21:45:24 +0100

    x

--- providers.tf
+++ providers.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "eu-west-1"
-  access_key = "AKIAXH6VHYU3XN4D35D7"
-  secret_key = "IOJOk1Tf9xkfeP8jek8d2Tju2dw4Ptzu2F1oEF33"
+  access_key = "<your-AWS-access-key>"
+  secret_key = "<your-AWS-secret-key>"
 }
